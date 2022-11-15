9d2c2c245a1f232079c9720c6dce452030f6b394
MasuyaYuto <basketball.1031msy@gmail.com>
2022-11-15 09:48:13 +0900

    課題8-3ページ遷移に関して実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,7 +11,7 @@
     <div class='posts'>
         @foreach ($posts as $post)
             <div class='post'>
-                <h2 class='title'>{{ $post->title}}</h2>
+                <a class='title' href="{{route('posts.show',$post->id)}}">{{ $post->title}}</a>
                 <p class='body'>{{$post->body}}</p>
             </div>
         @endforeach
